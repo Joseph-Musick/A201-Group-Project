ea025204ffa956884611da9f828573c2fa75bba8
King dude415 <97474293+Joseph-Musick@users.noreply.github.com>
2024-04-01 10:40:06 -0800

    Add files via upload

---
+++ Group Project main.cpp
@@ -0,0 +1,18 @@
+/****************************************************/
+/*			Name:				                    */
+/*			Class: CSCE A201 MWF					*/
+/*			File: Group Project						*/
+/*			Date:									*/
+/****************************************************/
+
+
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	return 0;
+}
